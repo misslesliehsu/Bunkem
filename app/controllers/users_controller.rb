381b81006de0b7c9213758d396464f73bb755e7b
ddbrennan <ddbren@gmail.com>
2018-01-02 16:29:04 -0500

    added signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user
     else
-      flash[:errors] << @user.errors.full_messages
+      flash[:errors] = @user.errors.full_messages
       redirect_to new_user_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "games#index"
-  resources :games
+  resources :games, except: :destroy
+  resources :users
 
   get "/login", to: "sessions#new", as: "signin"
   post "/sessions", to: "sessions#create"
